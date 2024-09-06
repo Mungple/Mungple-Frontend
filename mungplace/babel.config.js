f8d58ee49932c7e2f3ab5509dac4feb735e7f456
임경태 <limkt0925@gmail.com>
2024-09-06 17:39:38 +0900

    chore: install babel-plugin-module-resolver

--- mungplace/babel.config.js
+++ mungplace/babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  "plugins": [
+    ["module-resolver", {
+      "@": ["./src"],
+      "alias": {
+        "test": "./test",
+        "underscore": "lodash"
+      }
+    }]
+  ],
 };
