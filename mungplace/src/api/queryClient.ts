dd62c67373e3834020039c6dcd78e7d2fa69713f
임경태 <limkt0925@gmail.com>
2024-09-08 20:30:20 +0900

    feat: 화면 초기화

---
+++ mungplace/src/api/queryClient.ts
@@ -0,0 +1,18 @@
+import {QueryClient} from '@tanstack/react-query';
+
+// 모든 쿼리 관련 상태와 옵션 관리
+const queryClient = new QueryClient({
+  // 기본 설정 지정
+  defaultOptions: {
+    // 쿼리실행이 실패했을 때, 재시도하지 않도록 설정
+    queries: {
+      retry: false,
+    },
+    // 뮤테이션이 실패했을 때, 재시도하지 않도록 설정
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+export default queryClient;
