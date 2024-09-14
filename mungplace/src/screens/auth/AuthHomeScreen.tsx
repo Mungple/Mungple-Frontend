39108a9033e664a00ed212843d7bf077aff6fd25
임경태 <limkt0925@gmail.com>
2024-09-14 23:13:49 +0900

    [#16] feat: uninstall react-native-seoul

--- mungplace/src/screens/auth/AuthHomeScreen.tsx
+++ mungplace/src/screens/auth/AuthHomeScreen.tsx
@@ -2,50 +2,16 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Dimensions, Image, View} from 'react-native';
 
 import imageSource from '@/assets/mungple_logo.png'
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import CustomButton from '@/components/common/CustomButton';
 
 import {authNavigations} from '@/constants';
 import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-const AuthHomeScreen = () => {
-  const [naverLoginResponse, setNaverLoginResponse] = useState<NaverLoginResponse | null>(null);
-  const [kakaoLoginResponse, setKakaoLoginResponse] = useState<KakaoOAuthToken | null>(null);
-  const [kakaoProfile, setKakaoProfile] = useState<KakaoProfile | null>(null);
-
-  // SDK 초기화
-  useEffect(() => {
-    NaverLogin.initialize({
-      appName: 'com.mungplace',
-      consumerKey: `${process.env.NAVER_CUSTOMER_KEY}`,
-      consumerSecret: `${process.env.NAVER_CUSTOMER_SECRET}`,
-    });
-  }, []);
-
-  // 네이버 로그인 함수
-  const handleNaverLogin = async () => {
-    try {
-      const response = await NaverLogin.login();
-      setNaverLoginResponse(response);
-      console.log(response)
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // 카카오 로그인 함수
-  const handleKakaoLogin = async () => {
-    try {
-      const token = await NativeKakaoLogins.loginWithKakaoAccount();
-      setKakaoLoginResponse(token);
-
-      const profile = await NativeKakaoLogins.getProfile();
-      setKakaoProfile(profile);
-    } catch (err) {
-      console.log('Kakao login error:', err);
-    }
-  };
+type AuthHomeScreenProps = NativeStackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
 
 const AuthHomeScreen: React.FC<AuthHomeScreenProps> = ({navigation}) => {
   return (
@@ -61,37 +27,22 @@ const AuthHomeScreen: React.FC<AuthHomeScreenProps> = ({navigation}) => {
       <View style={styles.buttonContainer}>
         <CustomButton
           label="카카오 로그인하기"
-          onPress={handleKakaoLogin}
+          onPress={() => navigation.navigate(authNavigations.KAKAO)}
           style={styles.kakaoButtonContainer}
           textStyle={styles.buttonText}
-          icon={
-            <Ionicons name={'chatbubble-sharp'} color={'#181600'} size={16} />
-          }
         />
         <CustomButton
           label="네이버 로그인하기"
-          onPress={handleNaverLogin}
+          onPress={() => navigation.navigate(authNavigations.NAVER)}
           style={styles.naverButtonContainer}
           textStyle={styles.buttonText}
         />
         <CustomButton
           label="구글 로그인하기"
-          // onPress={()}
+          onPress={() => navigation.navigate(authNavigations.GOOGLE)}
           style={styles.googleButtonContainer}
           textStyle={styles.buttonText}
         />
-        {/* 네이버 로그인 결과 표시 */}
-        {naverLoginResponse && (
-          <View>
-            <Text>
-              네이버 로그인 성공:{' '}
-              {naverLoginResponse.successResponse?.accessToken}
-            </Text>
-            <Text>
-              {naverLoginResponse.successResponse?.accessToken}
-            </Text>
-          </View>
-        )}
       </View>
     </SafeAreaView>
   );
